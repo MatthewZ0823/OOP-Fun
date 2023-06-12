88ac06fae8e933b14a4802ada77a4cbc5bf108b1
depressed-fish <humberviewrobotics@gmail.com>
2023-06-12 12:27:02 -0400

    pokemon
    dgfjdj

---
+++ src/Move.java
@@ -0,0 +1,14 @@
+public class Move {
+   String moveName; 
+   double powerSomthing;
+public Move(String moveName, double powerSomthing){
+    this.moveName = moveName;
+    this.powerSomthing = powerSomthing;
+}
+public String getNameIdk(){
+    return moveName;
+}
+public double   getPoweSomthing(){
+    return powerSomthing;
+ }
+}
